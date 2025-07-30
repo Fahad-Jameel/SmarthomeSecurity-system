30425f2d79344944d2b60e92e47f2893299037ca
fahad-jameel <fahadjamil521@gmail.com>
2025-07-30 23:25:31 +0500

    frontend

--- server/middleware/async.js
+++
@@ -1,5 +0,0 @@
-// Async handler middleware to avoid try-catch blocks in controllers
-const asyncHandler = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { updateProfile } = require('../controllers/userController');
-const { protect } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.put('/profile', protect, updateProfile);
-
-module.exports = router;
